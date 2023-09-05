7a090a9a1aff3b775c8ab844ab670d7044b158f4
dimishpatriot <dimish@outlook.com>
2023-09-05 16:59:12 +0300

    add rest handlers

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var store = make(map[string]string)
@@ -38,5 +43,77 @@ func Delete(k string) error {
 	return nil
 }
 
+func kvPutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["key"]
+
+	v, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(k, string(v))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func kvGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["key"]
+
+	v, err := Get(k)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w,
+			err.Error(),
+			http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	_, _ = w.Write([]byte(v))
+}
+
+func kvDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["key"]
+
+	err := Delete(k)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w,
+			err.Error(),
+			http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/{key}", kvPutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", kvGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", kvDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
