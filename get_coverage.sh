aff19f872fbf7d1f42f8581ff40438f6eca255bd
dimishpatriot <dimish@outlook.com>
2023-09-20 14:18:22 +0300

    update coverage tool

--- get_coverage.sh
+++ get_coverage.sh
@@ -1,4 +1,4 @@
-go test ./... -coverprofile cover.out.tmp &&
-cat cover.out.tmp | grep -v "mock_" > cover.out &&
+go test -short -count=1 -race ./... -coverprofile cover.out.tmp &&
+cat cover.out.tmp | grep -v "mock_" | grep -v "interface.go" > cover.out &&
 rm cover.out.tmp &&
 go tool cover -func cover.out
